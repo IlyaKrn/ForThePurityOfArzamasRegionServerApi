24045bbf95b7b0569e9cd554b700653231c451b5
IlyaKrn <ilyakornoukhov@gmail.com>
2022-06-17 16:31:31 +0300

    LocateController

--- src/main/java/com/example/ForThePurityOfArzamasRegionServerApi/Domain/UseCases/Locate/GetLocateListAllUseCase.java
+++ src/main/java/com/example/ForThePurityOfArzamasRegionServerApi/Domain/UseCases/Locate/GetLocateListAllUseCase.java
@@ -2,17 +2,59 @@ package com.example.ForThePurityOfArzamasRegionServerApi.Domain.UseCases.Locate;
 
 import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.ImageRepository;
 import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.LocateRepository;
+import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.ProjectRepository;
+import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.ProjectRequestRepository;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.Image;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.Locate;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.Project;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.ProjectRequest;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.ImageResponse;
 import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.LocateResponse;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.ProjectRequestResponse;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.ProjectResponse;
 import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Support.ResponseModels.ResponseModel;
 
 import java.util.ArrayList;
 
 public class GetLocateListAllUseCase {
+
+    private LocateRepository locateRepository;
+    private ImageRepository imageRepository;
+
     public GetLocateListAllUseCase(LocateRepository locateRepository, ImageRepository imageRepository) {
 
     }
 
     public ResponseModel<ArrayList<LocateResponse>> execute() {
-        return null;
+        ResponseModel<ArrayList<LocateResponse>> response = new ResponseModel<>();
+        ArrayList<LocateResponse> projects = new ArrayList<>();
+        ArrayList<Locate> ps = (ArrayList<Locate>) locateRepository.findAll();
+        if(ps.size() > 0) {
+            for (Locate p : ps) {
+                ArrayList<ImageResponse> images = new ArrayList<>();
+                if (p.getImage_ids() != null && p.getImage_ids().length > 0) {
+                    for(Integer id : p.getImage_ids()) {
+                        ImageResponse img = null;
+                        try {
+                            Image i = imageRepository.findById(id).get();
+                            img = new ImageResponse(i.getId(), i.getUrl(), i.getHeight(), i.getWidth());
+                            images.add(img);
+                        } catch (Exception e) {
+                            images.add(null);
+                        }
+                    }
+                }
+
+                LocateResponse res = new LocateResponse(p.getId(), p.getTitle(), p.getMessage(), p.getUpload_time(), p.getLast_modified_time(), images, p.getChat_id(), p.getAddress(), p.getLongitude(), p.getLatitude());
+                projects.add(res);
+            }
+        }
+        else {
+            projects.add(null);
+            response.setResponse(projects);
+            return response;
+        }
+        response.setResponse(projects);
+        return response;
     }
 }
--- src/main/java/com/example/ForThePurityOfArzamasRegionServerApi/Domain/UseCases/Locate/GetLocateListByIdUseCase.java
+++ src/main/java/com/example/ForThePurityOfArzamasRegionServerApi/Domain/UseCases/Locate/GetLocateListByIdUseCase.java
@@ -2,16 +2,60 @@ package com.example.ForThePurityOfArzamasRegionServerApi.Domain.UseCases.Locate;
 
 import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.ImageRepository;
 import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.LocateRepository;
+import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.ProjectRepository;
+import com.example.ForThePurityOfArzamasRegionServerApi.Data.Repositories.ProjectRequestRepository;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.Image;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.Locate;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.Project;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.DatabaseModels.ProjectRequest;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.ImageResponse;
 import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.LocateResponse;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.ProjectRequestResponse;
+import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Data.ResponseModels.ProjectResponse;
 import com.example.ForThePurityOfArzamasRegionServerApi.Domain.Models.Support.ResponseModels.ResponseModel;
 
 import java.util.ArrayList;
 
+
+
 public class GetLocateListByIdUseCase {
+
+
+    private LocateRepository projectRepository;
+    private ProjectRequestRepository requestRepository;
+    private ImageRepository imageRepository;
+    private ArrayList<Integer> ids;
+
     public GetLocateListByIdUseCase(LocateRepository locateRepository, ImageRepository imageRepository, ArrayList<Integer> ids) {
     }
 
     public ResponseModel<ArrayList<LocateResponse>> execute() {
-        return null;
+        ResponseModel<ArrayList<LocateResponse>> response = new ResponseModel<>();
+        ArrayList<LocateResponse> projects = new ArrayList<>();
+        for(Integer id : ids) {
+            try {
+                Locate p = projectRepository.findById(id).get();
+                ArrayList<ImageResponse> images = new ArrayList<>();
+                if (p.getImage_ids() != null && p.getImage_ids().length > 0) {
+                    for(Integer id1 : p.getImage_ids()) {
+                        ImageResponse img = null;
+                        try {
+                            Image i = imageRepository.findById(id1).get();
+                            img = new ImageResponse(i.getId(), i.getUrl(), i.getHeight(), i.getWidth());
+                            images.add(img);
+                        } catch (Exception e) {
+                            images.add(null);
+                        }
+                    }
+                }
+
+                LocateResponse res = new LocateResponse(p.getId(), p.getTitle(), p.getMessage(), p.getUpload_time(), p.getLast_modified_time(), images, p.getChat_id(), p.getAddress(), p.getLongitude(), p.getLatitude());
+                projects.add(res);
+            } catch (Exception ignored){
+                projects.add(null);
+            }
+        }
+        response.setResponse(projects);
+        return response;
     }
 }
